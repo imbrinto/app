d2347ed3cb9fae33de1eb3ebc1b1b6ca54e4a164
imbrnto <brintoroy007@gmail.com>
2024-02-16 19:23:46 +0600

    project files

---
+++ bin/application990.dart
@@ -0,0 +1,5 @@
+import 'package:application990/application990.dart' as application990;
+
+void main(List<String> arguments) {
+  print('Hello world: ${application990.calculate()}!');
+}
---
+++ test/application990_test.dart
@@ -0,0 +1,8 @@
+import 'package:application990/application990.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
